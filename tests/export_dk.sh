b21f5208f0e12610afc94600f661658d96808c1b
bodeveix <bodeveix@irit.fr>
2026-09-07 14:25:28 +0200

    remove export

--- tests/export_dk.sh
+++ tests/export_dk.sh
@@ -44,7 +44,7 @@ do
         π/utf_path|escape_path|'a b/escape file'|require_nondkmident|262_pair_ex_2|require_symbol);;
         # use builtin strings
         # requires Tactic
-        1374|Tactic|assume|first_hyp|all_hyps|1493);;
+        1374|Tactic|assume|first_hyp|all_hyps|with_goal|1493);;
         # default case
         *) translate $f.lp;;
     esac
